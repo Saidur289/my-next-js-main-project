61c09bee3572e05108cc3177f3da7813c4ac3d3d
Saidur289 <saidur.riaz1@gmailcomm>
2025-03-02 15:26:34 +0600

    set up my next js project

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+
+
+export default function Home() {
+  return (
+    <>
+  <section className="w-full  bg-[#EE2B69] min-h-[530px]  flex justify-center items-center flex-col py-10 px-6 pattern">
+    <h1 className="uppercase bg-black px-6 py-3 font-work-sans font-extrabold text-white sm:text-[54px] sm:leading-[64px] text-[36px] leading-[46px] max-w-5xl text-center my-5g">Pitch Your Startup, <br />Connect With Entrepreneurs</h1>
+    <p className="sub-heading !max-w-3xl">Submit Ideas, Vote on pitches, and Get Noticed in Virtual Competitions.</p>
+    
+  </section>
+    </>
+  );
+}
