57f1dda2f32ef9152f25fc70aeaf5d0f5428421c
Saidur289 <saidur.riaz1@gmailcomm>
2025-03-13 00:46:36 +0600

    create full details page by ppr next js method

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,7 +1,13 @@
+
 import { defineQuery } from "next-sanity";
 
 export const STARTUPS_QUERY = defineQuery(`*[_type=='startup' && defined(slug.current) && !defined($search) || category match $search || author->name match $search ] | order(_createdAt desc){
   _id, title, image, _createdAt, 
   "author": author-> { _id, name, image, bio }, 
   slug, category, views, description
+}`)
+export const STARTUPS_BY_ID_QUERY= defineQuery(`*[_type=='startup' && _id==$id][0]{
+  _id, title, image, _createdAt, 
+  "author": author-> { _id, name,username, image, bio }, 
+  slug, category, views, description,pitch
 }`)
